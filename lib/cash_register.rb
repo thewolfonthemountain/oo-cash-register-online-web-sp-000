8d163165982b0006c1e32caac6e4110ed36de71c
Sean Langton <slangton1@gmail.com>
2018-11-07 21:06:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ attr_accessor :total, :discount
   end
 
   def apply_discount
-    @total = (@total*(1-@discount.0.class/100))
+    @total = (@total*(1-@discount.to_f/100))
     binding.pry
   end
 
