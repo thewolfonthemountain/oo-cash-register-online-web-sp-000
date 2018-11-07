eef4b8f9d2ebf282ed0149f991466ccebdc9f9c4
Sean Langton <slangton1@gmail.com>
2018-11-07 21:17:49 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,10 @@ attr_accessor :total, :discount, :items
   def add_item(name, price, quantity = 1)
     @total += price * quantity
     items_added = 0
-    do while items_to_add < quantity {
+    while items_to_add < quantity
       @items << name
       items_added += 1
-    }
+    end
   end
 
   def apply_discount
